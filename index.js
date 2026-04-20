a98d0e33976b1a8ecc479c91a03be9b7a35a3caf
Mao <maoacr@gmail.com>
2026-04-20 14:22:52 -0500

    Parametros de tipo Query

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { faker } = require("@faker-js/faker");
+
 const app = express();
 const port = 3000;
 
@@ -11,7 +13,17 @@ app.get("/new", (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-  res.json({ name: "Product 1", price: 100 });
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.url(),
+    });
+  }
+  res.json(products);
 });
 
 app.get("/products/:id", (req, res) => {
